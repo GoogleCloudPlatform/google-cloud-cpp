1bdf85dcae5694a08d391904cff95784d8b88815
Carlos O'Ryan <coryan@google.com>
2020-12-08 20:35:35 +0000

    feat(storage): implement "public access prevention"
    This is a new metadata field in the storage Bucket. This PR also fixes
    some typos and blunders in the existing tests for
    BucketIamConfiguration.

--- google/cloud/storage/internal/bucket_metadata_parser.cc
+++ google/cloud/storage/internal/bucket_metadata_parser.cc
@@ -135,6 +135,9 @@ StatusOr<BucketMetadata> BucketMetadataParser::FromJson(
       c.bucket_policy_only =
           ParseUniformBucketLevelAccess(config["bucketPolicyOnly"]);
     }
+    if (config.count("publicAccessPrevention") != 0) {
+      c.public_access_prevention = config.value("publicAccessPrevention", "");
+    }
     result.iam_configuration_ = c;
   }
 
@@ -300,6 +303,10 @@ std::string BucketMetadataToJsonString(BucketMetadata const& meta) {
       // The lockedTime field is not mutable and should not be set by the client
       // the server will provide a value.
       c["bucketPolicyOnly"] = std::move(bpo);
+    }
+    if (meta.iam_configuration().public_access_prevention.has_value()) {
+      c["publicAccessPrevention"] =
+          *meta.iam_configuration().public_access_prevention;
     }
     metadata_as_json["iamConfiguration"] = std::move(c);
   }
