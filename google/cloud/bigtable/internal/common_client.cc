3e64632f077516d4b36f470b65a682a93b8ca6fd
Marek Dopiera <marek@unoperate.com>
2020-12-30 19:21:33 +0000

    feat: Cancel all connection refresh timers in dtor
    This is a part of #2567.
    
    Timers created by `CommonClient` to refresh connections are now
    cancelled in the `CommonClient` dtor. This is needed to make sure that
    no stray timers remain in the completion queue after the destruction of
    `CommonClient`.

--- google/cloud/bigtable/internal/common_client.cc
+++ google/cloud/bigtable/internal/common_client.cc
@@ -22,7 +22,8 @@ namespace internal {
 ConnectionRefreshState::ConnectionRefreshState(
     std::chrono::milliseconds max_conn_refresh_period)
     : max_conn_refresh_period_(max_conn_refresh_period),
-      rng_(std::random_device{}()) {}
+      rng_(std::random_device{}()),
+      timers_(std::make_shared<OutstandingTimers>()) {}
 
 std::chrono::milliseconds ConnectionRefreshState::RandomizedRefreshDelay() {
   std::lock_guard<std::mutex> lk(mu_);
@@ -37,38 +38,77 @@ void ScheduleChannelRefresh(
     std::shared_ptr<grpc::Channel> const& channel) {
   // The timers will only hold weak pointers to the channel or to the
   // completion queue, so if either of them are destroyed, the timer chain
-  // will simply not continue. Unfortunately, that means that some stray
-  // timers may remain in the `CompletionQueue` for a while, but this is
-  // generally unavoidable because there is no way to cancel individual
-  // timers.
+  // will simply not continue.
   std::weak_ptr<grpc::Channel> weak_channel(channel);
   std::weak_ptr<CompletionQueue> weak_cq(cq);
   using TimerFuture = future<StatusOr<std::chrono::system_clock::time_point>>;
-  cq->MakeRelativeTimer(state->RandomizedRefreshDelay())
-      .then([weak_channel, weak_cq, state](TimerFuture fut) {
-        if (!fut.get()) {
-          // Timer cancelled.
-          return;
-        }
-        auto channel = weak_channel.lock();
-        if (!channel) return;
-        auto cq = weak_cq.lock();
-        if (!cq) return;
-        cq->AsyncWaitConnectionReady(channel, std::chrono::system_clock::now() +
-                                                  kConnectionReadyTimeout)
-            .then([weak_channel, weak_cq, state](future<Status> fut) {
-              auto conn_status = fut.get();
-              if (!conn_status.ok()) {
-                GCP_LOG(WARNING)
-                    << "Failed to refresh connection. Error: " << conn_status;
-              }
-              auto channel = weak_channel.lock();
-              if (!channel) return;
-              auto cq = weak_cq.lock();
-              if (!cq) return;
-              ScheduleChannelRefresh(cq, state, channel);
-            });
-      });
+  auto timer_future =
+      cq->MakeRelativeTimer(state->RandomizedRefreshDelay())
+          .then([weak_channel, weak_cq, state](TimerFuture fut) {
+            if (!fut.get()) {
+              // Timer cancelled.
+              return;
+            }
+            auto channel = weak_channel.lock();
+            if (!channel) return;
+            auto cq = weak_cq.lock();
+            if (!cq) return;
+            cq->AsyncWaitConnectionReady(
+                  channel,
+                  std::chrono::system_clock::now() + kConnectionReadyTimeout)
+                .then([weak_channel, weak_cq, state](future<Status> fut) {
+                  auto conn_status = fut.get();
+                  if (!conn_status.ok()) {
+                    GCP_LOG(WARNING) << "Failed to refresh connection. Error: "
+                                     << conn_status;
+                  }
+                  auto channel = weak_channel.lock();
+                  if (!channel) return;
+                  auto cq = weak_cq.lock();
+                  if (!cq) return;
+                  ScheduleChannelRefresh(cq, state, channel);
+                });
+          });
+  state->timers().RegisterTimer(std::move(timer_future));
+}
+
+void OutstandingTimers::RegisterTimer(future<void> fut) {
+  std::unique_lock<std::mutex> lk(mu_);
+  if (cancel_all_) {
+    lk.unlock();
+    fut.cancel();
+    return;
+  }
+
+  timers_.emplace_back(future<void>());
+  auto iter = --timers_.end();
+  lk.unlock();
+  std::weak_ptr<OutstandingTimers> weak_self(shared_from_this());
+  *iter = fut.then([weak_self, iter](future<void>) {
+    auto self = weak_self.lock();
+    if (!self) return;
+    std::lock_guard<std::mutex> lk(self->mu_);
+    if (self->cancel_all_) return;  // we deregistered all timers in bulk
+    self->timers_.erase(iter);
+  });
+}
+
+void OutstandingTimers::CancelAll() {
+  std::list<future<void>> to_cancel;
+  {
+    std::lock_guard<std::mutex> lk(mu_);
+    if (cancel_all_) {
+      // Already cancelled
+      return;
+    }
+    cancel_all_ = true;
+    // We don't want to fire the callbacks with the lock held to avoid
+    // deadlocks.
+    timers_.swap(to_cancel);
+  }
+  for (auto& fut : to_cancel) {
+    fut.cancel();
+  }
 }
 
 }  // namespace internal
--- google/cloud/bigtable/internal/common_client.h
+++ google/cloud/bigtable/internal/common_client.h
@@ -22,6 +22,7 @@
 #include "google/cloud/log.h"
 #include "google/cloud/status_or.h"
 #include <grpcpp/grpcpp.h>
+#include <list>
 
 namespace google {
 namespace cloud {
@@ -38,6 +39,21 @@ namespace internal {
  */
 std::chrono::seconds constexpr kConnectionReadyTimeout(10);
 
+class OutstandingTimers
+    : public std::enable_shared_from_this<OutstandingTimers> {
+ public:
+  // Register a timer. It will automatically deregister on completion.
+  void RegisterTimer(future<void> fut);
+  // Cancel all currently registered timers and all which will be registered in
+  // the future.
+  void CancelAll();
+
+ private:
+  std::mutex mu_;
+  bool cancel_all_ = false;         // GUARDED_BY(mu_)
+  std::list<future<void>> timers_;  // GUARDED_BY(mu_)
+};
+
 /**
  * State required by timers scheduled by `CommonClient`.
  *
@@ -49,11 +65,13 @@ class ConnectionRefreshState {
   explicit ConnectionRefreshState(
       std::chrono::milliseconds max_conn_refresh_period);
   std::chrono::milliseconds RandomizedRefreshDelay();
+  OutstandingTimers& timers() { return *timers_; }
 
  private:
   std::mutex mu_;
   std::chrono::milliseconds max_conn_refresh_period_;
   google::cloud::internal::DefaultPRNG rng_;
+  std::shared_ptr<OutstandingTimers> timers_;
 };
 
 /**
@@ -100,9 +118,8 @@ class CommonClient {
   ~CommonClient() {
     // This will stop the refresh of the channels.
     channels_.clear();
-    // TODO(2567): remove this call when the user will have to provide their own
-    // `CompletionQueues`
-    background_threads_->cq().CancelAll();
+    // This will cancel all pending timers.
+    refresh_state_->timers().CancelAll();
   }
 
   /**
