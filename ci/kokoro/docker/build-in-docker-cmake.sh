3f7177133dd3a50ed740a717a8f91bea45dcbd13
Carlos O'Ryan <coryan@google.com>
2021-01-29 00:14:53 +0000

    Rename API baseline and update scripts

--- ci/kokoro/docker/build-in-docker-cmake.sh
+++ ci/kokoro/docker/build-in-docker-cmake.sh
@@ -461,7 +461,7 @@ if [[ "${TEST_INSTALL:-}" = "yes" ]]; then
       "bigtable_client"
       "google_cloud_cpp_common"
       "google_cloud_cpp_grpc_utils"
-      "spanner_client"
+      "google_cloud_cpp_spanner"
       "storage_client"
       "pubsub_client"
     )
